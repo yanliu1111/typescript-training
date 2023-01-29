c1c94a3c8868cb395c2a17b00448ae4a7dad2e9c
yanliu1111 <liuyan0504@gmail.com>
2023-01-28 21:12:05 -0700

    lesson01

---
+++ lesson01/main.ts
@@ -0,0 +1,2 @@
+let username = "Yan";
+console.log(username);
---
+++ lessontest/src/main.ts
@@ -0,0 +1,9 @@
+let username = "Greg";
+console.log(username);
+
+let a = 12;
+let b = "6";
+let c = 2;
+
+console.log(a / b);
+console.log(c * b);
