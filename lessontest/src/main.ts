4c368a8a9df9b94528b910a8e40640d3536200cf
yanliu1111 <liuyan0504@gmail.com>
2023-02-26 23:50:06 -0700

    lesson9 Utility Types done

--- lessontest/src/main.ts
+++ lessontest/src/main.ts
@@ -648,5 +648,73 @@ const finalGrades: Record<Students, LetterGrades> = {
   Alice: "B",
   Dave: "C",
 };
-
+// Pick and Omit
 type AssignResult = Pick<Assignment, "studentId" | "grade">;
+const score: AssignResult = {
+  studentId: "k123",
+  grade: 85,
+};
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">;
+
+const preview: AssignPreview = {
+  studentId: "k123",
+  title: "Final Project",
+};
+
+// Exclude and Extract
+
+type adjustedGrade = Exclude<LetterGrades, "U">;
+
+type highGrades = Extract<LetterGrades, "A" | "B">;
+
+// Nonnullable
+
+type AllPossibleGrades = "Dave" | "John" | null | undefined;
+type NamesOnly = NonNullable<AllPossibleGrades>;
+
+// ReturnType
+
+//type newAssign = { title: string, points: number }
+
+const createNewAssign = (title: string, points: number) => {
+  return { title, points };
+};
+
+type NewAssign = ReturnType<typeof createNewAssign>;
+
+const tsAssign: NewAssign = createNewAssign("Utility Types", 100);
+console.log(tsAssign);
+
+// Parameters
+
+type AssignParams = Parameters<typeof createNewAssign>;
+
+const assignArgs: AssignParams = ["Generics", 100];
+
+const tsAssign2: NewAssign = createNewAssign(...assignArgs);
+console.log(tsAssign2);
+
+// Awaited - helps us with the ReturnType of a Promise
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      if (err instanceof Error) console.log(err.message);
+    });
+  return data;
+};
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users) => console.log(users));
