35dc81fbe05ba102e426a4348b26e2fa912aed05
yanliu1111 <liuyan0504@gmail.com>
2023-03-04 01:50:02 -0700

    complete but has bug, not check box

--- vite/vite-ts-project/src/main.ts
+++ vite/vite-ts-project/src/main.ts
@@ -1,1 +1,47 @@
 import "./css/style.css";
+import FullList from "./model/FullList";
+import ListTemplate from "./templates/ListTemplate";
+import ListItem from "./model/ListItem";
+
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const templates = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault();
+
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText) return; // checking the length property of the text value is not needed. An empty string evaluates as false, too.
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1; //if the list is empty, then the id is 1, otherwise, the id is the last item's id plus 1
+
+    const newItem = new ListItem(itemId.toString(), newEntryText); //create a new list item
+    //there was a checked value but its not required because we had default value inside of the constructor back in the list item.
+    // so we dont have to pass in that checked value, we do need to pass in the id and the item text, because we prefer not to have those empty strings
+
+    fullList.addItem(newItem); //add the new item to the list
+    templates.render(fullList); //render the list on the DOM
+  });
+
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener("click", (): void => {
+    fullList.clearList(); //clear the list in the model
+    templates.clear(); //clear the display which is our template as well
+  });
+
+  //above two are event listeners, submit and the clear items button so they do not happen riht away, but what does happen right away is the loading the list, so we need to call fullList.load() and templates.render(fullList) right away
+  fullList.load(); //if we don't pass the full list here, because an argument for full list was not provided, so after we defined those properly in our classes they guide us through the rest of our app
+  templates.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+//we can defer inside of the script tag of JS, this essentially says we're not going to run our JS until the DOM is loaded, so we make sure the elements exist before we try to interact with them
---
+++ vite/vite-ts-project/src/templates/ListTemplate.ts
@@ -0,0 +1,56 @@
+import FullList from "../model/FullList";
+interface DOMList {
+  ul: HTMLUListElement;
+  clear(): void;
+  render(fullList: FullList): void;
+}
+
+export default class ListTemplate implements DOMList {
+  ul: HTMLUListElement;
+
+  static instance: ListTemplate = new ListTemplate(); //why static? because I want to create a single instance of the class, so that the class can be used as a singleton, and keep referring to that instance, because we only have one list in our application
+
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement;
+  }
+
+  clear(): void {
+    this.ul.innerHTML = "";
+  } // clear the list on the DOM
+
+  render(fullList: FullList): void {
+    this.clear(); //clear the list on the DOM, we don't want to have duplicates
+    fullList.list.forEach((item) => {
+      //start to create the list items
+      const li = document.createElement("li") as HTMLLIElement;
+      li.className = "item";
+
+      const check = document.createElement("input") as HTMLInputElement;
+      check.type = "checkbox";
+      check.id = item.id;
+      check.tabIndex = 0; // why didnt refer to the underscore? because it is a getter, so it is a property, not a method
+      check.checked = item.checked; // boolean
+
+      check.addEventListener("change", () => {
+        item.checked = !item.checked;
+        fullList.save();
+      });
+
+      const label = document.createElement("label") as HTMLLabelElement;
+      label.htmlFor = item.id;
+      label.textContent = item.item;
+      li.append(label); //why need append? because we are appending to the li, not the ul. what is ul? it is the ul element on the DOM
+
+      const button = document.createElement("button") as HTMLButtonElement;
+      button.className = "button";
+      button.textContent = "X";
+      li.append(button);
+
+      button.addEventListener("click", () => {
+        fullList.removeItem(item.id);
+        this.render(fullList);
+      });
+      this.ul.append(li);
+    }); //strange, but they do have .list data inside
+  }
+}
