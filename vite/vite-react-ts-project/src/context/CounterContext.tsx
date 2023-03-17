7eaa4a6356f467eab69a1800c609c32ef41aecaf
yanliu1111 <liuyan0504@gmail.com>
2023-03-17 01:12:40 -0600

    Feat: class 15 finish useContext then improve func

---
+++ vite/vite-react-ts-project/src/context/CounterContext.tsx
@@ -0,0 +1,69 @@
+import { ChangeEvent, createContext, ReactElement, useReducer } from "react";
+//first step: instead of typeof initState, we can use the type StateType
+type StateType = { count: number; text: string };
+
+const initState: StateType = { count: 0, text: "" };
+
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+  NEW_INPUT,
+}
+//what is enum? https://www.typescriptlang.org/docs/handbook/enums.html
+
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE;
+  payload?: string;
+};
+const reducer = (state: StateType, action: ReducerAction): StateType => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
+    default:
+      throw new Error();
+  }
+};
+// build a custom hook to use the context
+const useCounterContext = (initState: StateType) => {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
+
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
+
+  const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value });
+  };
+
+  return { state, increment, decrement, handleTextInput };
+};
+//super convient if you hover the useCounterContext, you can see the return types
+type UseCounterContextType = ReturnType<typeof useCounterContext>;
+// set initial state value
+const initContextState: UseCounterContextType = {
+  state: initState, //set initial state
+  increment: () => {}, //set initial function
+  decrement: () => {}, //set initial function
+  handleTextInput: (e: ChangeEvent<HTMLInputElement>) => {}, //set initial function
+};
+export const CounterContext =
+  createContext<UseCounterContextType>(initContextState);
+
+type ChildrenType = {
+  children?: ReactElement | undefined;
+};
+//... rest parameter, has to be the last parameter, children has to be the first parameter
+export const CounterProvider = ({
+  children,
+  ...initState
+}: ChildrenType & StateType): ReactElement => {
+  return (
+    <CounterContext.Provider value={useCounterContext(initState)}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
