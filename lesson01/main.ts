9684400102c3bd7021f2f40f301f5dc220df7e9a
yanliu1111 <liuyan0504@gmail.com>
2023-09-27 08:40:05 -0600

    feat: review 2.0

--- lesson01/main.ts
+++ lesson01/main.ts
@@ -105,3 +105,103 @@ let year = document.getElementById('year') as HTMLSpanElement;
 let thisYear: string = new Date().getFullYear().toString();
 year.setAttribute('datetime', thisYear);
 year.textContent = thisYear;
+
+//class -> super
+class Coder {
+  //constructor parameters are public by default
+  //different modifiers
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = 'TS'
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+  public getAge() {
+    return `${this.name} is ${this.age} years old`; // accesses the age value inside of the class
+  }
+}
+
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+  public getLang() {
+    return `${this.name} knows ${this.lang}`; // can access the lang value because it is protected in the parent class
+  }
+}
+
+const peter = new WebDev('Mac', 'Peter', 'Rock', 24);
+console.log(peter.getAge());
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+  play(action: string) {
+    return `${this.name} is ${action} the ${this.instrument}`;
+  }
+}
+const Page = new Guitarist('Page', 'Guitar');
+console.log(Page.play('strums'));
+
+// static, track static in the class, not in the instance
+class Peeps {
+  static count: number = 0;
+  static getCount() {
+    return Peeps.count;
+  }
+  public id: number;
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count; //counts increment first, 1 instead of 0
+  }
+}
+//getcount can be called on the class as well as the instance
+// getter and setter
+class Bands {
+  private dataState: string[];
+  constructor() {
+    this.dataState = [];
+  }
+  public get data(): string[] {
+    return this.dataState; //if only get, it is read only, beacuse no setter
+  }
+
+  public set data(value: string[]) {
+    if (
+      Array.isArray(value) &&
+      value.every((elements) => typeof elements === 'string')
+    ) {
+      this.dataState = value;
+      return;
+    } else throw new Error('Param is not an array of string');
+  }
+}
+
+const myBands = new Bands();
+myBands.data = ['The Beatles', 'The Rolling Stones', 'The Who']; //setter
+console.log(myBands.data); //getter
+myBands.data = [...myBands.data, 'The Doors'];
+console.log(myBands.data);
+
+//lesson 7
