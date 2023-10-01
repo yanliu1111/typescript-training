15b2e60b013d0dc4b4632e4b09dd43d34767f616
yanliu1111 <liuyan0504@gmail.com>
2023-10-01 14:45:51 -0600

    feat: if i amnot happy i type ts

--- lesson01/main.ts
+++ lesson01/main.ts
@@ -262,3 +262,82 @@ const logStudentKey = (student: Student, key: keyof Student): void => {
   console.log(`Student ${key}: ${student[key]}`);
 };
 logStudentKey(student, 'GPA');
+
+//lesson 8 generics, ts does allow generics sometimes we simply don't know what types will passed into a function interface class type Alias, all of the above and generics allow us to provide a placceholder a type of variable.
+const stringEcho = (arg: string): string => arg;
+const echo = <T>(arg: T): T => arg;
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === 'object' && arg !== null && !Array.isArray(arg);
+};
+// some logic some thinking about what needs to return
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg }; //!!arg is to convert to boolean same as Boolean(arg)
+};
+
+interface HasID {
+  id: number;
+}
+
+const getUsersProperty = <T extends HasID, K extends keyof T>(
+  users: T[],
+  key: K
+): T[K][] => {
+  return users.map((user) => user[key]);
+};
+const usersArray = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    address: {
+      street: 'Victor Plains',
+      suite: 'Suite 879',
+      city: 'Wisokyburgh',
+      zipcode: '90566-7771',
+      geo: {
+        lat: '-43.9509',
+        lng: '-34.4618',
+      },
+    },
+    phone: '010-692-6593 x09125',
+    website: 'anastasia.net',
+    company: {
+      name: 'Deckow-Crist',
+      catchPhrase: 'Proactive didactic contingency',
+      bs: 'synergize scalable supply-chains',
+    },
+  },
+];
+
+console.log(getUsersProperty(usersArray, 'email'));
+console.log(getUsersProperty(usersArray, 'username'));
